06ed36f23cee846b5e891470c5096e8345eab4a3
maksimtimofeev561-coder <maksimtimofeev561@gmail.com>
2025-10-03 01:27:52 +0300

    hash.java

--- hash.java
+++ hash.java
@@ -1,1 +1,14 @@
+import java.util.HashMap;
 
+public class SimpleExample {
+    public static void main(String[] args) {
+        HashMap<String, Integer> myMap = new HashMap<>();
+        
+        myMap.put("apple", 10);
+        myMap.put("banana", 20);
+        myMap.put("orange", 15);
+        for (String fruit : myMap.keySet()) {
+            System.out.println(fruit + ": " + myMap.get(fruit));
+        }
+    }
+}
